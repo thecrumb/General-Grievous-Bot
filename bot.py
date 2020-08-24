23d424526043e30add96b12617b05e94cf25588f
Christopher Sien <imchrissien@gmail.com>
2020-08-24 16:04:53 -0700

    commands don't really work. trying to figure it out

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ from config import token
 
 client = commands.Bot(command_prefix = '.')
 
-mode = 'text'
-
 @client.event
 async def on_ready():
     print('Bot is ready.')
@@ -14,18 +12,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if 'hello there' in message.content.lower():
-        if mode == 'text':
-            await message.channel.send('General Kenobi')
-        else:
-            await message.channel.send('https://gfycat.com/freshgleamingfulmar')
+        await message.channel.send('https://gfycat.com/freshgleamingfulmar')
 
-@client.command()
-async def mode(ctx, medium=None):
-    if medium not in (None, 'text', 'gif'):
-        await ctx.send('Please enter a valid mode.')
-        return
-    if medium != None:
-        mode = medium
-    await ctx.send(f'Current mode: {mode}')
+# @client.command()
+# async def ping(ctx):
+#     await ctx.send('Pong!')
+
+# @client.command()
+# async def mode(ctx, medium=None):
+#     if medium not in (None, 'text', 'gif'):
+#         await ctx.send('Please enter a valid mode.')
+#         return
+#     if medium != None:
+#         client.state = medium
+#     await ctx.send(f'Current mode: {client.state}')
 
 client.run(token)
