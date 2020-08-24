417f2d6ea6c0692b0c6be398a741a4c2534e34c1
Christopher Sien <imchrissien@gmail.com>
2020-08-24 16:09:50 -0700

    removed clutter

--- bot.py
+++ bot.py
@@ -14,17 +14,4 @@ async def on_message(message):
     if 'hello there' in message.content.lower():
         await message.channel.send('https://gfycat.com/freshgleamingfulmar')
 
-# @client.command()
-# async def ping(ctx):
-#     await ctx.send('Pong!')
-
-# @client.command()
-# async def mode(ctx, medium=None):
-#     if medium not in (None, 'text', 'gif'):
-#         await ctx.send('Please enter a valid mode.')
-#         return
-#     if medium != None:
-#         client.state = medium
-#     await ctx.send(f'Current mode: {client.state}')
-
 client.run(token)
