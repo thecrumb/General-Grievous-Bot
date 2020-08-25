658ec3bfc5436e4118998f894707c40a5b31dd5b
Christopher Sien <imchrissien@gmail.com>
2020-08-24 17:32:05 -0700

    fixed commands by adding last line to on_message

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from config import token
 
 client = commands.Bot(command_prefix = '.')
 
+client.state = 'text'
+
 @client.event
 async def on_ready():
     print('Bot is ready.')
@@ -12,6 +14,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if 'hello there' in message.content.lower():
-        await message.channel.send('https://gfycat.com/freshgleamingfulmar')
+        if client.state == 'text':
+            await message.channel.send('General Kenobi')
+        else:
+            await message.channel.send('https://gfycat.com/freshgleamingfulmar')
+    await client.process_commands(message)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@client.command()
+async def mode(ctx, medium=None):
+    if medium not in (None, 'text', 'gif'):
+        await ctx.send('Please enter a valid mode.')
+        return
+    if medium != None:
+        client.state = medium
+    await ctx.send(f'Current mode: {client.state}')
 
 client.run(token)
